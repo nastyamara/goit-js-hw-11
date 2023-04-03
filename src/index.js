ba061136dec90f3cd0a4ac59dc0a08d01954b7fd
nastyamara <mashamarmar@gmail.com>
2023-04-03 19:52:23 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { getImage } from "./getImages";
 let pagecount = 1;
 let hits = 0;
 let totalHits = 0;
+let search = "";
+const imagesOnPage = 40;
 
 const form = document.querySelector('form');
 const input = document.querySelector('input');
@@ -13,10 +15,13 @@ const gallery = document.querySelector('.gallery');
 const btn = document.querySelector(".load-more");
 form.addEventListener('submit', onSubmitBtnClick);
 btn.addEventListener('click', onLoadMoreBtnClick);
+btn.classList.add("visually-hidden");
 
-let search = "";
+ const simpleLightbox = new SimpleLightbox('.gallery a', {
+          captionsData: 'alt',
+          captionDelay: 250, })
 
-function onSubmitBtnClick(e) {
+async function onSubmitBtnClick(e) {
   hits = 0;
   btn.classList.add("visually-hidden")
   e.preventDefault();
@@ -24,53 +29,58 @@ function onSubmitBtnClick(e) {
   gallery.innerHTML = "";
   console.log(search);
   pagecount = 1;
+
+
   
-  if (search !== "") {
-    getImage(search, pagecount).then(images => {
+  try {if (search !== "") {
+   const images = await getImage(search, pagecount);
       renderImageCard(images.hits);
+      simpleLightbox.refresh();
       if (images.totalHits === 0) {
         Notiflix.Notify.failure('Sorry, there are no images matching your search query.Please try again.');
         return
-      } else if (images.totalHits >= 1) {
+      } else if (images.totalHits >= 1 && images.totalHits <= imagesOnPage) {
+       totalHits = images.totalHits;
+        console.log(totalHits);
+        Notiflix.Notify.success(`"Hooray! We found ${totalHits} images."`);
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results."); } 
+      else if(images.totalHits > imagesOnPage){
         totalHits = images.totalHits;
         console.log(totalHits);
         Notiflix.Notify.success(`"Hooray! We found ${totalHits} images."`)
-
-       const simpleLightbox = new SimpleLightbox('.gallery a', {
-          captionsData: 'alt',
-          captionDelay: 250,
-        }).refresh();
         pagecount += 1;
         btn.classList.remove("visually-hidden")
       }
-    }).catch((error) => {
+    }}
+  catch(error){
       console.log(error)
       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
       gallery.innerHTML = "";
       search = "";
-    });
-
+    };
   }
-}
+
     
-function onLoadMoreBtnClick() {
+async function onLoadMoreBtnClick() {
   console.log(search)
   console.log(hits);
-try{ getImage(search, pagecount).then(images => {
-    renderImageCard(images.hits);
-  })
-    const simpleLightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
+  try {
+  const images = await getImage(search, pagecount)
+  renderImageCard(images.hits);
+  simpleLightbox.refresh();
+
   pagecount += 1;
-  if(totalHits<= hits){Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")}}
+  if (totalHits <= (pagecount-1)* 40) {
+    console.log(pagecount)
+    btn.classList.add("visually-hidden");
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
  catch(error){console.log(error)}
 }
 
 function renderImageCard(images) {
-  
-  let imagesMarkup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+   let imagesMarkup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
     hits += 1;
     return `<div class="photo-card">
    <a class="gallery__link" href="${largeImageURL}">
